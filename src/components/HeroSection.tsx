2ec7cbc9f7eb229b9f3f545da75dd32f65f9a023
HenryEmmanuel06 <ebongemmanuel197@gmail.com>
2025-05-31 00:43:33 +0100

    resolving errors

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -3,7 +3,6 @@
 import Image from "next/image";
 import Link from "next/link";
 import { useState } from "react";
-import { motion } from "framer-motion";
 
 const HeroSection = () => {
   const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
