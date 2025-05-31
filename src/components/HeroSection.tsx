04e8f47e6f93ccfcba8a2ce25cccf180adfe6b2b
HenryEmmanuel06 <ebongemmanuel197@gmail.com>
2025-05-31 11:47:24 +0100

    changed something

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -2,7 +2,7 @@
 
 import Image from "next/image";
 import Link from "next/link";
-import { useState, useEffect } from "react";
+import { useState } from "react";
 import { motion, useScroll, useTransform } from "framer-motion";
 
 const HeroSection = () => {
