9dd73cb14bc20a8f759991f2be7efde2a8706928
HenryEmmanuel06 <ebongemmanuel197@gmail.com>
2025-06-21 14:33:44 +0100

    commit

--- src/components/ServicesSection.tsx
+++ src/components/ServicesSection.tsx
@@ -6,10 +6,10 @@ const services = [
   { title: "Website Development", col: 1, row: 1 },
   { title: "Apps Development", col: 2, row: 1 },
   { title: "UI & Interface Design", col: 1, row: 2 },
-  { title: "Site Management & Optimization", col: 2, row: 2 },
+  { title: "Product Design", col: 2, row: 2 },
   { title: "CMS Solutions {Wordpress, Webflow, Shopify}", col: 1, row: 3 },
   { title: "MVP Development for Startups", col: 2, row: 3 },
-  { title: "Product Design", col: 1, row: 4 },
+  { title: "Site Management & Optimization", col: 1, row: 4 },
   { title: "Branding {Identity, Strategy & Positioning}", col: 2, row: 4 },
 ];
 
@@ -27,17 +27,17 @@ const ServicesSection = () => {
       <p className="text-white/80 text-lg max-w-900px mb-12 text-center">
         From development to Result. We deliver skills crafted to position your brand for long-term impact.
       </p>
-      <div className="grid grid-cols-1 md:grid-cols-2 gap-6 w-full max-w-5xl mb-10 mx-auto w-[90%] max-w-[1180px]">
+      <div className="grid grid-cols-2 gap-6 w-full max-w-5xl mb-10 mx-auto w-[90%] max-w-[1180px]">
         {services.map((service, idx) => {
           // Determine if this is left or right column on desktop
           const isLeft = idx % 2 === 0;
           const borderRadius = isLeft
-            ? "rounded-full md:rounded-tr-[8px] md:rounded-br-[8px] md:rounded-tl-full md:rounded-bl-full"
-            : "rounded-full md:rounded-tl-[8px] md:rounded-bl-[8px] md:rounded-tr-full md:rounded-br-full";
+            ? "rounded-[8px] md:rounded-full md:rounded-tr-[8px] md:rounded-br-[8px] md:rounded-tl-full md:rounded-bl-full"
+            : "rounded-[8px] md:rounded-full md:rounded-tl-[8px] md:rounded-bl-[8px] md:rounded-tr-full md:rounded-br-full";
           return (
             <div
               key={idx}
-              className={`bg-white/5 px-8 py-5 text-white text-[16px] md:text-[20px] font-normal flex items-center justify-start shadow-md border border-[0.5px] border-[#FFFFFF33] border-opacity-10 backdrop-blur-[1px] ${borderRadius}`}
+              className={`bg-white/5 px-8 py-5 text-white text-[14px] md:text-[20px] font-normal flex items-center justify-start shadow-md border border-[0.5px] border-[#FFFFFF33] border-opacity-10 backdrop-blur-[1px] text-center md:text-left ${borderRadius}`}
               style={{
                 boxShadow: "0 0 10px rgba(0, 0, 0, 0.15)",
               }}
