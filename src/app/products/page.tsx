659132f19cea5c03825b5540c99f91676d2eda4c
HenryEmmanuel06 <ebongemmanuel197@gmail.com>
2025-06-21 14:49:33 +0100

    products page

--- src/app/products/page.tsx
+++ src/app/products/page.tsx
@@ -13,7 +13,7 @@ export default function Products() {
         <p className="text-white/80 text-lg max-w-2xl mx-auto mb-12">
           We have many years of experience working for big brands both directly and indirectly through big Agencies. We partner with you and your team to deliver technology solutions.
         </p>
-        <div className="flex justify-center rounded-full flex-wrap gap-4 mb-20 px-[25px] py-[20px] border border-[0.5px] border-[#232323] w-[90%] max-w-[800px] mx-auto">
+        <div className="flex justify-center rounded-full overflow-x-auto gap-4 mb-20 px-[25px] py-[20px] border-none md:border border-[0.5px] border-[#232323] w-[100%] max-w-[800px] mx-auto">
           {categories.map((category) => (
             <button
               key={category}
@@ -25,11 +25,12 @@ export default function Products() {
           ))}
         </div>
 
+
+      </div>
+      
         <div className="w-[90%] max-w-[1270px] backdrop-blur-[1.5px] bg-[#FFFFFF1A] flex justify-center items-center mx-auto mt-20 text-center text-white py-[100px] px-[220px] rounded-lg">
           <h1 className='text-8xl mx-auto text-center font-bold'>COMING SOON</h1>
         </div>
-
-      </div>
     </main>
   );
 } 
